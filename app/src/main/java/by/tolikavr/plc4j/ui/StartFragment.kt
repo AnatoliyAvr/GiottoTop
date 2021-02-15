e16af85c868333e4aa9db0a210f0da5a1faff205
tolikavr <anatoliy.avr@gmail.com>
2021-02-15 17:26:12 +0300

    add spinner

--- app/src/main/java/by/tolikavr/plc4j/ui/StartFragment.kt
+++ app/src/main/java/by/tolikavr/plc4j/ui/StartFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import by.tolikavr.plc4j.R
 import by.tolikavr.plc4j.databinding.StartFragmentBinding
+import by.tolikavr.plc4j.utilits.APP_ACTIVITY
+
 
 class StartFragment : Fragment() {
 
@@ -20,13 +24,15 @@ class StartFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View {
     _binding = StartFragmentBinding.inflate(layoutInflater, container, false)
-    return mBinding.root
+    val view = mBinding.root
+    return view
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
     viewModel = ViewModelProvider(this).get(StartViewModel::class.java)
-    mBinding.tvMode.setText(R.string.modeAuto)
+
+    //mBinding.tvMode.setText(R.string.modeAuto)
   }
 
 
