bb260b4b3c04929c25f6e010e05acf612d186fe6
tolikavr <anatoliy.avr@gmail.com>
2021-04-07 14:30:31 +0300

    viewModel delete test flow

--- app/src/main/java/by/tolikavr/plc4j/ui/start/StartViewModel.kt
+++ app/src/main/java/by/tolikavr/plc4j/ui/start/StartViewModel.kt
@@ -1,17 +1,5 @@
 package by.tolikavr.plc4j.ui.start
 
-import androidx.annotation.MainThread
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
-class StartViewModel : ViewModel() {
-
-  private val _initStart = MutableStateFlow(true)
-  val initStart: StateFlow<Boolean> = _initStart.asStateFlow() // только для чтения
-
-  fun setInitStart(state: Boolean) {
-    _initStart.value = state
-  }
-}
+class StartViewModel : ViewModel() {}
